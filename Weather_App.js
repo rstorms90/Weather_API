9bbb350c06644fcd16fd107a7b178bc638210b34
Russ Storms <rstorms90@gmail.com>
2018-09-14 16:38:19 -0600

    Working toggle button

--- Weather_App.js
+++ Weather_App.js
@@ -33,102 +33,106 @@ function updateUI(response2) {
     humidity
   } = response2.data.current
 
-  let moreLess = true
-  let degrees = null
-
   let feelsLikeF = Math.round(response2.data.current.feelslike_f)
   let feelsLikeC = Math.round(response2.data.current.feelslike_c)
   let fahrenheit = Math.round(response2.data.current.temp_f)
-  let celcius = Math.round(response2.data.current.temp_c)
+  let celsius = Math.round(response2.data.current.temp_c)
   
-  let changeList = document.getElementById(`moreData`)
-  let america = document.getElementsByClassName(`america`)
-  let world = document.getElementsByClassName(`world`)
+  let moreLess = true
+  let degrees = true
+  // let changeList = document.getElementById(`moreData`)
   let weatherWords = weather.split(` `)
 
 
-
-  // Toggle ºC & ºF
-  function changeDegrees() {
-    if (degrees === true) {
-      degrees === false;
-    } else {
-      degrees === true;
-    }
-    if (degrees === true) {
-      document.getElementById(`degType`).innerHTML = `Show ºC`
-      document.getElementById(`temp`).innerHTML = fahrenheit + `º`
-      world.classList.replace(`on`, `off`)
-      america.classList.replace(`off`, `on`)
-    } else {
-      document.getElementById(`degType`).innerHTML = `Show ºF`
-      document.getElementById(`temp`).innerHTML = celcius + `º`
-      america.classList.replace(`off`, `on`)
-      world.classList.replace(`on`, `off`)
-    }
-  }
-
-
-                // APPEND TO MORE DATA BUTTON
-              //   <p class="world" id="windKm"></p>
-              //   <p class="world" id="precipMm"></p>
-              //   <p class="world" id="feelsLikeC"></p>
-              //   <p class="world" id="visKm"></p>
-              //   <p class="america" id="windMph"></p>
-              //   <p class="america" id="precipIn"></p>
-              //   <p class="america" id="feelsLikeF"></p>
-              //   <p class="america" id="visMiles"></p>
-              //   <p id="windDir"></p>
-              //   <p id="humidity"></p>
-
-  // Toggle More & Less Data
-  function changeMoreLess() {
-    if (moreLess === true) {
-      moreLess === false;
-    } else {
-      moreLess === true;
-    }
-    if (moreLess === true) {
-      moreLess.innerHTML = `More Data`
-      changeList.style.opacity = `0`
-    } else {
-      moreLess.innerHTML = `Less Data`
-      changeList.style.opacity = `1`
-    }
-  }
+  
 
   // Use location to determine initial unit type
-  if (`america`) {
+  if (`United States of America`) {
     degrees === false;
     changeDegrees();
   } else {
     degrees === true;
     changeDegrees();
   }
-
-
+  
   // Display user's local temperature & conditions
   location.innerHTML = location
   moreLess.innerHTML = `More Data`
   weather.innerHTML = weather
-  windMph.innerHTML = `Wind Speed: ` + windMph + ` Mph`
-  windKm.innerHTML = `Wind Speed: ` + windKm + ` Km/h`
-  windDir.innerHTML = `Wind Direction: ` + windDir
-  precipMm.innerHTML = `Precipitation: ` + precipMm
-  precipIn.innerHTML = `Precipitation: ` + precipIn
-  humidity.innerHTML = `Humidity: ` + humidity
-  feelsLikeF.innerHTML = `Feels Like: ` + feelsLikeF + `º`
-  feelsLikeC.innerHTML = `Feels Like: ` + feelsLikeC + `º`
-  visMiles.innerHTML = `Visibility: ` + visMiles + ` Mi`
-  visKm.innerHTML = `Visibility: ` + visKm + ` Km`
+  // windMph.innerHTML = `Wind Speed: ` + windMph + ` Mph`
+  // windKm.innerHTML = `Wind Speed: ` + windKm + ` Km/h`
+  // windDir.innerHTML = `Wind Direction: ` + windDir
+  // precipMm.innerHTML = `Precipitation: ` + precipMm
+  // precipIn.innerHTML = `Precipitation: ` + precipIn
+  // humidity.innerHTML = `Humidity: ` + humidity
+  // feelsLikeF.innerHTML = `Feels Like: ` + feelsLikeF + `º`
+  // feelsLikeC.innerHTML = `Feels Like: ` + feelsLikeC + `º`
+  // visMiles.innerHTML = `Visibility: ` + visMiles + ` Mi`
+  // visKm.innerHTML = `Visibility: ` + visKm + ` Km`
 
-  // 'Click' functions for toggle buttons
-  document.getElementById(`degType`)
-  degType.addEventListener(`click`, function(e) {
-    changeDegrees()
-  })
-  
-  moreLess.addEventListener(`click`, function(e) {
-    changeMoreLess()
-  })
+
+
+
+
+
+    // 'Click' functions for toggle buttons
+    let degType = document.getElementById(`degType`)
+    let temp = document.getElementById(`temp`)
+
+    // Toggle ºC & ºF
+    degType.addEventListener(`click`, function(e) {
+      e.preventDefault()
+      if (degType.innerText === `Show ºF`) {
+        degType.innerText = `Show ºC`
+        temp.innerText = fahrenheit + `º`
+      } else {
+        degType.innerText = `Show ºF`
+        temp.innerText = celsius + `º`
+      }
+    })
+
+  function changeDegrees() {
+    if (degrees === true) {
+      //World `OFF`
+      //America `ON`
+      document.getElementById(`degType`).innerText = `Show ºC`
+      document.getElementById(`temp`).innerText = fahrenheit + `º`
+
+    } else {
+      //World `ON`
+      //America `OFF`
+      document.getElementById(`degType`).innerText = `Show ºF`
+      document.getElementById(`temp`).innerText = celcius + `º`
+
+    }
+  }
+
+
+    // Toggle More & Less Data
+    // moreLess.addEventListener(`click`, function(e) {
+    //   e.preventDefault()
+    //   changeMoreLess()
+    // })
+
+  // function changeMoreLess() {
+  //   if (moreLess === true) {
+  //     moreLess.innerHTML = `More Data`
+  //     changeList.style.opacity = `0`
+  //   } else {
+  //     moreLess.innerHTML = `Less Data`
+  //     changeList.style.opacity = `1`
+  //   }
+  // }
+          // APPEND TO MORE DATA BUTTON
+        //   <p class="world" id="windKm"></p>
+        //   <p class="world" id="precipMm"></p>
+        //   <p class="world" id="feelsLikeC"></p>
+        //   <p class="world" id="visKm"></p>
+        //   <p class="america" id="windMph"></p>
+        //   <p class="america" id="precipIn"></p>
+        //   <p class="america" id="feelsLikeF"></p>
+        //   <p class="america" id="visMiles"></p>
+        //   <p id="windDir"></p>
+        //   <p id="humidity"></p>
+
 }
