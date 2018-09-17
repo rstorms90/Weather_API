5962b91331065f202fca0620b74e82ac0f437cd5
Russ Storms <rstorms90@gmail.com>
2018-09-17 16:06:11 -0600

    Added forecast dates and more styling

--- Weather_App.js
+++ Weather_App.js
@@ -12,7 +12,7 @@ function getCurrentWeather(response) {
   let long = coords[1]
 
   //API for JSON object containing user's current weather data
-  let api = `https://api.apixu.com/v1/forecast.json?key=e452323a9db841b187b164113180709&q=` + lat + `,` + long + `&days=7`
+  let api = `https://api.apixu.com/v1/forecast.json?key=e452323a9db841b187b164113180709&q=` + lat + `,` + long + `&days=6`
 
   axios.get(api)
     .then(e => updateUI(e))
@@ -20,11 +20,10 @@ function getCurrentWeather(response) {
 
 
 function updateUI(response2) {
-  let location = response2.data.location.name
-  let state = response2.data.location.region
+  let curLocation = response2.data.location.name
+  let country = response2.data.location.country
   let current = response2.data.current.condition.code
   let weatherCondition = response2.data.current.condition.text
-
   let {
     visMiles,
     visKm,
@@ -40,6 +39,30 @@ function updateUI(response2) {
   let feelsLikeC = Math.round(response2.data.current.feelslike_c)
 
 
+  //Append current location
+  let location = document.getElementById(`location`)
+  if (country = `United States of America`) {
+    country = `US`
+  }
+  location.innerText = curLocation + `, ` + country
+
+
+
+  //Append 6 day forecast weather
+  let forecastRow = document.getElementById(`forecast`)
+  let forecastCol = document.getElementById(`col`)
+  let forecast = response2.data.forecast.forecastday
+
+  for (let i = 0; i < forecast.length; i++) {
+    let date = forecast[i].date
+    let day = document.createElement(`div`)
+    day.setAttribute(`class`, `col-md-2`)
+    day.innerText = date
+    forecastRow.appendChild(day)
+  }
+
+  
+
   // Declare temp variables
   let degTypeF = document.getElementById(`degTypeF`)
   let degTypeC = document.getElementById(`degTypeC`)
@@ -50,14 +73,13 @@ function updateUI(response2) {
   degTypeC.innerText = `ºC`
 
 
-
   //Set default to fahrenheit, gray out celsius
     temp.innerText = fahrenheit + `º`
     degTypeF.classList.add(`on`)
     degTypeC.classList.add(`off`)
   
 
-  // Toggle ºC & ºF (button)
+  // Toggle ºC & ºF (buttons)
   degTypeF.addEventListener(`click`, function(e) {
     temp.innerText = fahrenheit + `º`
     degTypeF.classList.replace(`off`, `on`)
@@ -69,5 +91,6 @@ function updateUI(response2) {
     degTypeF.classList.replace(`on`, `off`)
     degTypeC.classList.replace(`off`, `on`)
   })
+
 
 }
